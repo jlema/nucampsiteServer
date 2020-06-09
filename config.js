098c3e26df63322516da59a78ff516021ffafa9f
John (Juan) Lema <jlema@users.noreply.github.com>
2020-06-08 23:29:23 -0700

    Added OAuth with passport and Facebook Using passport-facebook-token package

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '4b5ec528-35b3-52d5-aed2-1a1a623fa1d9',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '585910162332915',
+        clientSecret: '020897caa09bc34008d2c122d0b497b5'
+    }
 }
--- routes/cors.js
+++ routes/cors.js
@@ -3,13 +3,15 @@ const cors = require('cors');
 const whitelist = ['http://localhost:3000', 'https://localhost:3443'];
 const corsOptionsDelegate = (req, callback) => {
     let corsOptions;
+    let corsError = null;
     console.log(req.header('Origin'));
     if (whitelist.indexOf(req.header('Origin')) !== -1) {
         corsOptions = { origin: true };
     } else {
         corsOptions = { origin: false };
+        corsError = new Error('Cross-Origin Resource Sharing (CORS) not allowed on this endpoint!');
     }
-    callback(null, corsOptions);
+    callback(corsError, corsOptions);
 };
 
 exports.cors = cors();
